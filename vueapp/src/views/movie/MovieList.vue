a2ee85291398daefa2e30d0a7bced7a97b4077dc
wangyuan1021 <wyy_yxdz@163.com>
2019-07-07 15:57:40 +0800

    vueapp

---
+++ vueapp/src/views/movie/MovieList.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+  <li>
+  <div class="movie-img">
+  <img :src="movie.images.small" alt="">
+  </div>
+  <div class="movie-desc">
+  <h2>{{movie.title}}</h2>
+  <p><观众评分 <span class="aver">{{movie.rating.average}} </span></p>
+  主演：<span v-for="actor in movie.casts">{{actors.name}} </span>
+  </div>
+  </li>
+</div>
+</template>
+<script>
+  export default {
+
+    props:['movie']
+  }
+</script>
+<style>
+
+</style>
